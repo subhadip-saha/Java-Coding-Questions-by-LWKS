c8b7d0e803bf31d87875acbc266a89619b839777
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-04-11 07:17:07 +0530

    How to compare two arrays in java?

---
+++ Array Questions/LWKS40_CompareTwoArrays.java
@@ -0,0 +1,18 @@
+//How to compare two arrays in java?
+
+package array_questions;
+
+import java.util.Arrays;
+
+public class LWKS40_CompareTwoArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr1[] = {10,20,30,40,50};
+		int arr2[] = {10,20,30,40,50};
+		
+		System.out.println(Arrays.equals(arr1, arr2));
+	}
+
+}
